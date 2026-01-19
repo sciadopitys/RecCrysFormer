31a64df72073fc38b6ccb5b8fb61f983b9f5eb3c
sciadopitys <31415786+sciadopitys@users.noreply.github.com>
2026-01-19 14:04:49 -0600

    Added comments for recycling batch generation script

--- dataset_rec.py
+++ dataset_rec.py
@@ -12,6 +12,7 @@ os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 import time 
 
+# rounding to nearest even number
 def round2( n ): 
   
     # Smaller multiple 
@@ -25,6 +26,8 @@ def round2( n ):
 
 
 import random
+
+# shuffle indices within a specified range of a list
 def shuffle_slice(a, start, stop):
     i = start
     while (i < (stop-1)):
@@ -34,26 +37,29 @@ def shuffle_slice(a, start, stop):
 
 
 def create_batches(gau = False, rep = 0.0):
-    
+
+    # read in sorted list of training set example ids
     with open("train_examples.txt") as myfile1: 
         trainlist = myfile1.readlines()
     trainlist  = [x.rstrip() for x in trainlist]
-    
+
+    # read in list of indices separating examples of different tensor dimensions
     with open("size_indices.txt") as myfile: 
         sindices = myfile.readlines()
     sindices  = [x.rstrip() for x in sindices]
-    
+
+    # shuffle examples within tensor dimension bins
     for i in range(len(sindices) - 1):
         start = int(sindices[i])
         end = int(sindices[i+1])
         shuffle_slice(trainlist, start, end)
         
-
+    # read in list of indices defining effective training batches
     with open("training_indices.txt") as myfile2: 
         indices = myfile2.readlines()
     indices  = [x.rstrip() for x in indices]
     
-
+    # create each training batch
     for i in range(len(indices) - 1):
         start = int(indices[i])
         end = int(indices[i+1])
@@ -61,10 +67,12 @@ def create_batches(gau = False, rep = 0.0):
         pslist = []
         ylist = []
         for j in range(start, end):
+
+            # load in Patterson map
             new_x = torch.load('patterson_pt_scaled/' + trainlist[j] + '_patterson.pt')
             new_x = torch.unsqueeze(new_x, 0)
 
-            
+            # randomly determine whether to use refined template map or raw previous model's prediction
             replace = False
             r = random.random()
             if r <= rep:
@@ -78,6 +86,7 @@ def create_batches(gau = False, rep = 0.0):
                 new_x3 = torch.load('predictions_15_angle/' + trainlist[j] + '.pt')
                 new_x3 = new_x3[0,0,:,:,:]  
 
+            # if specified, add Gaussian noise to central region of template map
             if gau:
                 shape = new_x3.shape
                 a = round2(0.85*shape[0])
@@ -105,19 +114,25 @@ def create_batches(gau = False, rep = 0.0):
             
             new_x3 = torch.unsqueeze(new_x3, 0)
             
-            
             new_xlist = torch.load('res/' + trainlist[j] + '_ps.pt')  
-            
+
+            # concatenate Patterson and template tensors
             new_xcomb = torch.cat((new_x, new_x3), 0)
+          
             xlist.append(new_xcomb)
             pslist.append(new_xlist)
+
+            # load in ground truth electron density
             new_y = torch.load('electron_density_pt_scaled/' + trainlist[j] + '_fft.pt')
             new_y = torch.unsqueeze(new_y, 0)
             ylist.append(new_y)
-        
+
+        # combine corresponding tensors
         data_x = torch.stack(xlist)
         data_ps = torch.stack(pslist)
         data_y = torch.stack(ylist)
+
+        # save tensors defining training batch
         torch.save(data_x, 'patterson_pt_scaled/train_' + str(i) + '_patterson.pt')  
         torch.save(data_ps, 'patterson_pt_scaled/train_' + str(i) + '_ps.pt')
         torch.save(data_y, 'electron_density_pt_scaled/train_' + str(i) + '.pt')
